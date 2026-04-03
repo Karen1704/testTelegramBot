dd948c1fe7d653f5e51812a7c8a2d72b524f881c
Karen Ghalachyan <karen.ghalachyan@skillex.am>
2026-04-03 17:50:58 +0400

    fix

--- scripts/setWebhook.ts
+++ scripts/setWebhook.ts
@@ -2,7 +2,7 @@ import { bot } from "../src/bot.js";
 import { env } from "../src/utils/env.js";
 
 async function setWebhook() {
-    const url = `${process.env.WEBHOOK_URL ? `https://${process.env.WEBHOOK_URL}` : "http://localhost:3000"}/api/webhook`;
+    const url = `${process.env.WEBHOOK_URL ? `${process.env.WEBHOOK_URL}` : "http://localhost:3000"}/api/webhook`;
     try {
         const result = await bot.api.setWebhook(url, {
             secret_token: env.WEBHOOK_SECRET
